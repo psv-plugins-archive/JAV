db21229a25cd703b45544c8de7c8389187384816
浅倉麗子 <asakurareiko@protonmail.ch>
2019-12-03 16:55:47 -0500

    Add SceBt consts and prototypes

---
+++ sce_bt.h
@@ -0,0 +1,56 @@
+#ifndef SCE_BT_H
+#define SCE_BT_H
+
+typedef struct {
+// 0x0
+	int mac0;
+	int mac1;
+	int bt_class;
+	int bt_profile;
+// 0x10
+	int unk_10;
+	short vid;
+	short pid;
+	int unk_18;
+	int unk_1C;
+// 0x20
+	char name[0x80];
+	char unk_A0[0x60];
+// 0x100
+} SceBtRegisteredInfo;
+
+extern int sceBtGetRegisteredInfo(int mac0, int mac1, SceBtRegisteredInfo *reg_info, int count);
+#define BT_PROFILE_A2DP  0xC
+#define BT_PROFILE_AVRCP 0x30
+#define BT_PROFILE_HID   0xC0
+#define BT_PROFILE_HSP   0x30000
+
+#define BT_CLASS_COMPUTER 1
+#define BT_CLASS_PHONE    2
+#define BT_CLASS_HEADSET  3
+#define BT_CLASS_SPEAKER  4
+#define BT_CLASS_MOUSE    5
+#define BT_CLASS_KEYBOARD 6
+#define BT_CLASS_PRINTER  7
+#define BT_CLASS_GAMEPAD  8
+#define BT_CLASS_REMOTE   9
+
+extern int sceBtGetConnectingInfo(int mac0, int mac1);
+#define BT_CON_DISCONNECTED        1
+#define BT_CON_CONNECTING          2
+#define BT_CON_CONNECTED_AUDIO_OUT 5
+#define BT_CON_CONNECTED           6
+
+extern int sceBtAvrcpReadVolume(int mac0, int mac1);
+extern int sceBtAvrcpSendVolume(int mac0, int mac1, int vol);
+
+#define SCE_BT_ERROR_AVCTP_OPEN_NO_L2C     0x802F0601
+#define SCE_BT_ERROR_AVCTP_SEND_NO_L2C     0x802F0603
+#define SCE_BT_ERROR_AVCTP_NOT_CONNECTED   0x802F0604
+#define SCE_BT_ERROR_AVCTP_SEND_BUSY       0x802F0605
+#define SCE_BT_ERROR_AVCTP_SEND_NO_PRESS   0x802F0606
+#define SCE_BT_ERROR_AVCTP_SEND_NO_RELEASE 0x802F0607
+#define SCE_BT_ERROR_AVCTP_READ_NO_VOLUME  0x802F0608
+#define SCE_BT_ERROR_AVCTP_SEND_NOT_RUBY   0x802F0609
+
+#endif
