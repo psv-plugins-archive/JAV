a2f10f61171fb3751a4784efdbd86532065c9164
浅倉麗子 <asakurareiko@protonmail.ch>
2020-05-05 00:13:14 -0400

    Apply copyright notice to all files

--- kernel_module/main.c
+++ kernel_module/main.c
@@ -1,6 +1,6 @@
 /*
-自動オーディオボリューム
-Copyright (C) 2019-2020 浅倉麗子
+This file is part of JAV 自動オーディオボリューム
+Copyright © 2019, 2020 浅倉麗子
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
